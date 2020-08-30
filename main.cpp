7a2b5541914977f7e1a66ede27b93e747810b191
sammoth <sammoth.th@gmail.com>
2020-08-30 13:37:24 +0100

    version number

--- main.cpp
+++ main.cpp
@@ -1,3 +1,3 @@
 #include "stdafx.h"
-DECLARE_COMPONENT_VERSION("Scream Output","0.1","Scream protocol network output");
+DECLARE_COMPONENT_VERSION("Scream Output","0.2-beta","Scream protocol network output");
 VALIDATE_COMPONENT_FILENAME("foo_out_scream.dll");
